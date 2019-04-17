1004fc23274e2e60bc4054fc6eb81ecf26568c3b
Olivier Chan Sion Moy <olivierchanmoy@yahoo.com>
2019-04-16 19:47:32 -0700

    Update Dollar.h

--- Lab 01/CIS22C-Lab01-Olivier_Chan_Luis_Guerrero/CIS22C-Lab01-Olivier_Chan_Luis_Guerrero/Dollar.h
+++ Lab 01/CIS22C-Lab01-Olivier_Chan_Luis_Guerrero/CIS22C-Lab01-Olivier_Chan_Luis_Guerrero/Dollar.h
@@ -4,7 +4,7 @@
 class Dollar : public Currency
 {
 public:
-	Dollar(int whole, int fractional) : currencyNote("Dollar") {}
+	Dollar(int whole, int fractional) : Currency("Dollar", whole, fractional) {}
 	//Currency(std::string cNote, std::string cNotePlural, std::string cCoin, std::string cCoinPlural, int whole, int fractional)
 		//:currencyNote(cNote), currencyNotePlural(cNotePlural), currencyCoin(cCoin), currencyCoinPlural(cCoinPlural), wholeParts(whole), fractionalParts(fractional) {}
 	~Dollar();
