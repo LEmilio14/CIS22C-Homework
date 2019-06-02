434cf47f85dc69facfcd609aa08ccb1a3eb662d9
olivec <olivierchanmoy@yahoo.com>
2019-06-02 01:00:23 -0700

    Initial work on Lab 5

---
+++ Lab 05/BST_oc_lg/BST_oc_lg/BST.h
@@ -0,0 +1,103 @@
+/********************************************************************************
+************************************ BST ****************************************
+*********************************************************************************
+* This class is a BST (binary search tree) ADT.
+*****************
+***** USAGE *****
+*****************
+* 
+*********************************************************************************/
+
+#pragma once
+
+#include <iostream>
+
+#include "BST_Node.h"
+
+template <typename T>
+class BST
+{
+protected:
+	int count;
+	BST_Node<T>* head;
+
+	BST_Node<T>* insert(T&, BST_Node<T>*);
+public:
+	BST();
+	virtual ~BST();
+	/**
+	* getHead
+	*
+	* @brief Returns head.
+	*
+	* @return head
+	*/
+	BST_Node<T>* getHead();
+	/**
+	* insert
+	*
+	* @brief Recursively inserts the data into a new node in the sorted tree.
+	*
+	* @param The data to insert, as a reference.
+	*/
+	void insert(T&);
+};
+
+template <typename T>
+BST<T>::BST()
+{
+	count = 0;
+	head = nullptr;
+}
+
+template <typename T>
+BST<T>::~BST()
+{
+	if (head != nullptr)
+	{
+		delete head;
+		head = nullptr;
+	}
+}
+
+template <typename T>
+BST_Node<T>* BST<T>::getHead()
+{
+	return head;
+}
+template <typename T>
+void BST<T>::insert(T& d)
+{
+	insert(d, head);
+}
+
+template <typename T>
+BST_Node<T>* BST<T>::insert(T& d, BST_Node<T>* root)
+{
+	//The BST is completely empty
+	if (head == nullptr)
+	{
+		head = new BST_Node<T>(d);
+		return head;
+	}
+	
+	//If the root is non-existent, create a new node and return it - the new node will be saved in the previous call
+	if (root == nullptr)
+	{
+		return new BST_Node<T>(d);
+	}
+
+	//If the new data is less than root, recur for left root
+	if (d < root->data)
+	{
+		root->left = insert(d, root->left);
+	}
+	//If the new data is greater or equal than root, recur for right root
+	else
+	{
+		root->right = insert(d, root->right);
+	}
+
+	//Return the root so previous recursion calls keep their current values for left and right
+	return root;
+}
---
+++ Lab 05/BST_oc_lg/BST_oc_lg/BST_Node.h
@@ -0,0 +1,49 @@
+/********************************************************************************
+********************************** BST_Node *************************************
+*********************************************************************************
+* This class is part of a BST (binary search tree) abstract data structure.
+* The purpose of this class is to act as a data-containing node in a tree structure,
+* with each node having a branch to two deeper nodes.
+*****************
+***** USAGE *****
+*****************
+* This class should only be used within the BST class.
+* Creating a BST_Node must be done with a reference to the contained data.
+* The left and right nodes may be given as parameters or set using functions.
+*********************************************************************************/
+
+#pragma once
+
+template <typename T>
+struct BST_Node
+{
+public:
+	T& data;
+	BST_Node<T>* left;
+	BST_Node<T>* right;
+
+	BST_Node(T&, BST_Node<T>* = nullptr, BST_Node<T>* = nullptr);
+	virtual ~BST_Node();
+};
+
+template <typename T>
+BST_Node<T>::BST_Node(T& d, BST_Node<T>* l, BST_Node<T>* r) : data(d), left(l), right(r)
+{
+
+}
+
+template <typename T>
+BST_Node<T>::~BST_Node()
+{
+	if (left != nullptr)
+	{
+		delete left;
+		left = nullptr;
+	}
+
+	if (right != nullptr)
+	{
+		delete right;
+		right = nullptr;
+	}
+}
